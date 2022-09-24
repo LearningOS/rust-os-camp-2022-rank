a79013787b60b30f1e1b65a541aaa83223ff6fb3
Campbell He <duskmoon314@users.noreply.github.com>
2022-09-24 09:32:32 +0800

    Update classroom's meta

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -2,10 +2,10 @@ import * as dotenv from "dotenv";
 
 export const config = dotenv.config();
 
-export const organiztion: string = 'os-autograding';            // 组织
-export const fullOrganization: string = '113154735-os-autograding-classroom-a857a2'; // classroom名称
-export const assignment = 'oskernel';   // assignment
-export const works = ['default', 'main']; // assignment 的不同情况
+export const organiztion: string = 'LearningOS';            // 组织
+export const fullOrganization: string = '19380377-rust-os-camp-2022'; // classroom名称
+export const assignment = 'rust-os-camp-2022';   // assignment
+export const works = ['lab1', 'lab2', 'lab3', 'lab4', 'lab5']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 
 // JsonData store the grades and the other info.
